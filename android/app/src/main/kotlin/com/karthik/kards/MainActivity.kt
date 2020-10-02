6afbbf651430367c63dc25fce83a9993b0736ad9
kargna <kargna@outlook.com>
2020-10-01 21:27:10 -0700

    first commit

---
+++ android/app/src/main/kotlin/com/karthik/kards/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.karthik.kards
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/dashboard.dart
@@ -0,0 +1,192 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Dashboard extends StatefulWidget {
+  @override
+  _DashboardState createState() => _DashboardState();
+}
+
+class _DashboardState extends State<Dashboard> {
+  @override
+  Widget build(BuildContext context) {
+    // to get size
+    var size = MediaQuery.of(context).size;
+
+    // style
+    var cardTextStyle = TextStyle(
+        fontSize: 14,
+        color: Color.fromRGBO(63, 63, 63, 1));
+
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[
+          Container(
+            height: size.height * .3,
+            color: Colors.brown,
+          ),
+          SafeArea(
+            child: Padding(
+              padding: EdgeInsets.all(16.0),
+              child: Column(
+                children: <Widget>[
+                  Container(
+                    height: 64,
+                    margin: EdgeInsets.only(bottom: 20),
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        CircleAvatar(
+                          radius: 32,
+                          backgroundImage: NetworkImage(
+                              'https://lh3.googleusercontent.com/ogw/ADGmqu8qTwX1e3BNf2w7mqlLVkh7KHubuB_BC3TbxDbK=s192-c-mo'),
+                        ),
+                        SizedBox(
+                          width: 16,
+                        ),
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text(
+                              'Karthik Gnanakumar',
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 25),
+                            ),
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                    child: GridView.count(
+                      mainAxisSpacing: 10,
+                      crossAxisSpacing: 10,
+                      primary: false,
+                      crossAxisCount: 2,
+                      children: <Widget>[
+                        Card(
+                          shape:RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(8)
+                          ),
+                          elevation: 4,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              Icon(
+                                Icons.view_carousel,
+                              ),
+                              Text(
+                                'View Decks',
+                                style: cardTextStyle,
+                              )
+                            ],
+                          ),
+                        ),
+
+                        Card(
+                          shape:RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(8)
+                          ),
+                          elevation: 4,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              Icon(
+                                Icons.add,
+                              ),
+                              Text(
+                                'Create Deck',
+                                style: cardTextStyle,
+                              )
+                            ],
+                          ),
+                        ),
+
+                        Card(
+                          shape:RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(8)
+                          ),
+                          elevation: 4,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              Icon(
+                                Icons.insert_chart,
+                              ),
+                              Text(
+                                'Analytics',
+                                style: cardTextStyle,
+                              )
+                            ],
+                          ),
+                        ),
+
+                        Card(
+                          shape:RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(8)
+                          ),
+                          elevation: 4,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              Icon(
+                                Icons.people,
+                              ),
+                              Text(
+                                'Friends',
+                                style: cardTextStyle,
+                              )
+                            ],
+                          ),
+                        ),
+
+                        Card(
+                          shape:RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(8)
+                          ),
+                          elevation: 4,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              Icon(
+                                Icons.account_circle,
+                              ),
+                              Text(
+                                'Account',
+                                style: cardTextStyle,
+                              )
+                            ],
+                          ),
+                        ),
+
+                        Card(
+                          shape:RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(8)
+                          ),
+                          elevation: 4,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              Icon(
+                                Icons.settings,
+                              ),
+                              Text(
+                                'Settings',
+                                style: cardTextStyle,
+                              )
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
